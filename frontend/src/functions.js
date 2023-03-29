1a8a3e315fe967f6dc0be36646a420dac05a5991
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-03-29 11:31:02 +0530

    added readme file

---
+++ frontend/src/functions.js
@@ -0,0 +1,11 @@
+// function App() {
+//   return "hii";
+// }
+// function Sum(a, b) {
+//   return a + b;
+// }
+// module.exports = { App, Sum };
+
+export function add(x, y) {
+  return x + y;
+}
